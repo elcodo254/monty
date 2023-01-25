e8f3f9e079ede14ad82d3858edda10700853d01b
elcodo254 <ellawachaga@gmail.com>
2023-01-25 14:26:10 -0800

    code

--- pall_func.c
+++ pall_func.c
@@ -49,6 +49,12 @@ void pchar(stack_t **stack, unsigned int line_number)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+	c = (*stack)->n;
+	if (c < 0 || c > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 	putchar(c);
 	putchar('\n');
 }
